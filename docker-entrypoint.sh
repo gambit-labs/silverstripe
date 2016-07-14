f2a2aa2e51c09e39724297da030f5a3ffecef44f
Lucas Käldström <lucas.kaldstrom@multi.fi>
2016-07-14 14:45:46 +0300

    Many changes. Renamed DEST_DIR to WWW_DIR. Added RW mode and patching of SilverStripe. Added SS_ERROR_LOG variable and performance-related nginx options. Added HTTPS support and php timeout duration is now configurable. Removes install.php if a repo is present

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
-# SOURCE_DIR and DEST_DIR comes from an ENV variable in the Dockerfile
-# Copy everything except cms, framework and _ss_environment.php from ${SOURCE_DIR} to ${DEST_DIR}
+# SOURCE_DIR and WWW_DIR comes from an ENV variable in the Dockerfile
+# Copy everything except cms, framework and _ss_environment.php from ${SOURCE_DIR} to ${WWW_DIR}
 if [[ $(ls ${SOURCE_DIR}) != "" ]]; then
-	(cd ${SOURCE_DIR} && cp -R $(ls ${SOURCE_DIR} | grep -v 'framework' | grep -v 'cms' | grep -v '_ss_environment.php') ${DEST_DIR})
+	(cd ${SOURCE_DIR} && cp -R $(ls ${SOURCE_DIR} | grep -v 'framework' | grep -v 'cms' | grep -v '_ss_environment.php') ${WWW_DIR})
 fi
 
 # dev mode exposes all web data at the /live mount.
 # to use it, start the docker container normally, but set DEV_MODE=1 to enable it and mount the host dir you want to see the live content to /live
 # Example: Expose the web content at $(pwd)/live, where $(pwd) is the SilverStripe project root: 
-# docker run -d --net host -e DEV_MODE=1 -v $(pwd):/source -v $(pwd)/live:/live {image-name}
+# docker run -d -e DEV_MODE=1 -v $(pwd):/source -v $(pwd)/live:/live {image-name}
 DEV_MODE=${DEV_MODE:-0}
 DEV_DIR=${DEV_DIR:-/live}
 if [[ ${DEV_MODE} == 1 ]]; then
-	(cd ${DEST_DIR} && cp -R ${DEST_DIR}/* ${DEV_DIR})
-	rm -r ${DEST_DIR}
-	ln -s ${DEV_DIR} ${DEST_DIR}
+	(cd ${WWW_DIR} && cp -R ${WWW_DIR}/* ${DEV_DIR})
+	rm -r ${WWW_DIR}
+	ln -s ${DEV_DIR} ${WWW_DIR}
+fi
+
+# readwrite mode copies the official source into the repo you're working on
+# the site is hosted from the directory on the host
+# WARNING: This will modify your current work directory
+# Example: docker run -d -e RW_MODE=1 -v $(pwd):/source {image-name}
+RW_MODE=${RW_MODE:-0}
+if [[ ${RW_MODE} == 1 && ${DEV_MODE} == 0 ]]; then
+	rm -rf ${SOURCE_DIR}/cms ${SOURCE_DIR}/framework ${SOURCE_DIR}/_ss_environment.php
+
+	# Do not override existing files with the same name
+	cp -r --no-clobber ${WWW_DIR}/* ${SOURCE_DIR}
+	rm -r ${WWW_DIR}
+	ln -s ${SOURCE_DIR} ${WWW_DIR}
+fi
+
+# Traverse SilverStipe patches, and apply them
+if [[ -d ${WWW_DIR}/_patches/${SILVERSTRIPE_VERSION} ]]; then
+	for file in ${WWW_DIR}/_patches/${SILVERSTRIPE_VERSION}/*.patch; do
+		patch -p1 < $file
+	done
+	for file in ${WWW_DIR}/_patches/${SILVERSTRIPE_VERSION}/*.gitapply; do
+		git apply < $file
+	done
 fi
 
 # Runtime configuration options
@@ -26,12 +50,20 @@ SS_DATABASE_USERNAME=${SS_DATABASE_USERNAME:-root}
 SS_DATABASE_PASSWORD=${SS_DATABASE_PASSWORD:-root}
 SS_DEFAULT_ADMIN_USERNAME=${SS_DEFAULT_ADMIN_USERNAME:-admin}
 SS_DEFAULT_ADMIN_PASSWORD=${SS_DEFAULT_ADMIN_PASSWORD:-admin}
+SS_ERROR_LOG=${SS_ERROR_LOG:-silverstripe.errlog}
 
 SS_TIMEZONE=${SS_TIMEZONE:-"Europe/Helsinki"}
-SS_WEB_HOST=${SS_WEB_HOST:-localhost}
+SS_DOMAIN_NAME=${SS_DOMAIN_NAME:-localhost}
 SS_LISTEN_PORT=${SS_LISTEN_PORT:-80}
+SS_LISTEN_HTTPS_PORT=${SS_LISTEN_HTTPS_PORT:-443}
+SS_ENABLE_HTTPS=${SS_ENABLE_HTTPS:-0}
 
-cat > ${DEST_DIR}/_ss_environment.php <<EOF
+SS_WORKER_PROCESSES=${SS_WORKER_PROCESSES:-2}
+SS_WORKER_CONNECTIONS=${SS_WORKER_CONNECTIONS:-1024}
+SS_MAX_EXECUTION_TIME=${SS_MAX_EXECUTION_TIME:-300}
+SS_MAX_UPLOAD_SIZE=${SS_MAX_UPLOAD_SIZE:-32}
+
+cat > ${WWW_DIR}/_ss_environment.php <<EOF
 <?php
 ini_set('date.timezone', '${SS_TIMEZONE}');
 define('SS_ENVIRONMENT_TYPE', '${SS_ENVIRONMENT_TYPE}');
@@ -41,17 +73,57 @@ define('SS_DATABASE_USERNAME', '${SS_DATABASE_USERNAME}');
 define('SS_DATABASE_PASSWORD', '${SS_DATABASE_PASSWORD}');
 define('SS_DEFAULT_ADMIN_USERNAME', '${SS_DEFAULT_ADMIN_USERNAME}');
 define('SS_DEFAULT_ADMIN_PASSWORD', '${SS_DEFAULT_ADMIN_PASSWORD}');
+define('SS_ERROR_LOG', '${SS_ERROR_LOG}');
 global \$_FILE_TO_URL_MAPPING;
-\$_FILE_TO_URL_MAPPING['${DEST_DIR}'] = 'http://${SS_WEB_HOST}';
+\$_FILE_TO_URL_MAPPING['${WWW_DIR}'] = 'http://${SS_DOMAIN_NAME}';
 EOF
 
 # Replace dynamic values in the default web site config
-sed -e "s|/var/www|${DEST_DIR}|g" -i /etc/nginx/sites-available/default
-sed -e "s|localhost|${SS_WEB_HOST}|g" -i /etc/nginx/sites-available/default
-sed -e "s|80|${SS_LISTEN_PORT}|g" -i /etc/nginx/sites-available/default
+sed -e "s|SS_WORKER_PROCESSES|${SS_WORKER_PROCESSES}|g" -i /etc/nginx/nginx.conf
+sed -e "s|SS_WORKER_CONNECTIONS|${SS_WORKER_CONNECTIONS}|g" -i /etc/nginx/nginx.conf
+sed -e "s|SS_MAX_UPLOAD_SIZE|${SS_MAX_UPLOAD_SIZE}|g" -i /etc/nginx/nginx.conf
+
+sed -e "s|SS_MAX_EXECUTION_TIME|${SS_MAX_EXECUTION_TIME}|g" -i /etc/nginx/php.conf
+
+sed -e "s|max_execution_time = 30|max_execution_time = ${SS_MAX_EXECUTION_TIME}|g" -i /etc/php5/fpm/php.ini
+sed -e "s|upload_max_filesize = 2M|upload_max_filesize = ${SS_MAX_UPLOAD_SIZE}M|g" -i /etc/php5/fpm/php.ini
+sed -e "s|post_max_size = 3M|post_max_size = $((SS_MAX_UPLOAD_SIZE+1))M|g" -i /etc/php5/fpm/php.ini
+
+
+# Require those two files
+# docker run -d -e SS_ENABLE_HTTPS=1 -v $(pwd)/certs:/certs {image_name}
+if [[ ${SS_ENABLE_HTTPS} == 1 && (! -f ${CERT_DIR}/site.crt || ! -f ${CERT_DIR}/site.key) ]]; then
+
+	echo "Fatal error: Tried to start in https mode but ${CERT_DIR}/site.crt or ${CERT_DIR}/site.key does not exist."
+	echo "Those two files are required in order to enable https."
+	echo "Exiting..."
+	exit 1
+fi
+
+sed -e "s|SS_ROOT_DIR|${WWW_DIR}|g" -i /etc/nginx/sites-available/default-http
+sed -e "s|SS_DOMAIN_NAME|${SS_DOMAIN_NAME}|g" -i /etc/nginx/sites-available/default-http
+sed -e "s|SS_LISTEN_PORT|${SS_LISTEN_PORT}|g" -i /etc/nginx/sites-available/default-http
+
+sed -e "s|SS_ROOT_DIR|${WWW_DIR}|g" -i /etc/nginx/sites-available/default-https
+sed -e "s|SS_DOMAIN_NAME|${SS_DOMAIN_NAME}|g" -i /etc/nginx/sites-available/default-https
+sed -e "s|SS_LISTEN_PORT|${SS_LISTEN_PORT}|g" -i /etc/nginx/sites-available/default-https
+sed -e "s|SS_LISTEN_HTTPS_PORT|${SS_LISTEN_HTTPS_PORT}|g" -i /etc/nginx/sites-available/default-https
+sed -e "s|CERT_DIR|${CERT_DIR}|g" -i /etc/nginx/sites-available/default-https
+
+# If https is disabled, remove the nginx config for HTTPS
+if [[ ${SS_ENABLE_HTTPS} == 1 ]]; then
+	ln -s /etc/nginx/sites-available/default-https /etc/nginx/sites-enabled/default
+else
+	ln -s /etc/nginx/sites-available/default-http /etc/nginx/sites-enabled/default
+fi
+
+# Remove install.php where the $database variable is set in _config.php, i.e. remove install.php from all real projects
+if [[ ! -z $(grep "global \$database" ${WWW_DIR}/mysite/_config.php) && -z $(grep "$database = ''" ${WWW_DIR}/mysite/_config.php) ]]; then
+	rm -f ${WWW_DIR}/install.php
+fi
 
 # Make the user and group www-data own the content. nginx is using that user for displaying content 
-chown -R www-data:www-data ${DEST_DIR}
+chown -R www-data:www-data ${WWW_DIR}
 
 # Start the FastCGI server
 exec php5-fpm &
